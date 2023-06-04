a27631669bd591fd656a06681fd146d959423449
Harin Rana <harinrana24@gmail.com>
2023-06-04 13:37:29 -0400

    Updates

---
+++ precache-manifest.de70af6f8d00322b5077a3c154b6ed73.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9814826f594800172430",
+    "url": "/Weather-App/static/js/main.9814826f.chunk.js"
+  },
+  {
+    "revision": "f08d440b840239294cc3",
+    "url": "/Weather-App/static/js/1.f08d440b.chunk.js"
+  },
+  {
+    "revision": "edcfa8a852f959e2feef",
+    "url": "/Weather-App/static/js/runtime~main.edcfa8a8.js"
+  },
+  {
+    "revision": "95c6cea011a71adb6343bbe7f0685043",
+    "url": "/Weather-App/index.html"
+  }
+];
